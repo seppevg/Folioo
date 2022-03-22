4f187ab2f4132fb59732fab397f35743a020830d
grietbeyens <beyensgriet@gmail.com>
2022-03-22 22:33:31 +0100

    feat: finished frontend log in and register

--- login.php
+++ login.php
@@ -11,48 +11,65 @@
 </head>
 
 <body class="form-background">
-    <div>
-        <!-- <img src="./assets/Folioo.png" alt="Foolio Logo" class="logo"> -->
-
-        <div class="form">
-            <form action="" method="post">
-                <h3 class="form-title">Login</h3>
-
-                <div class="form-field">
-                    <label class="form-label" for="email">Email</label>
-                    <input name="email" autocomplete="off" class="form-input" type="email" placeholder="someone@thomasmore.be">
-                </div>
-
-                <div class="form-field">
-                    <label class="form-label" for="password">Password</label>
-                    <input name="password" autocomplete="off" class="form-input" type="password" placeholder="123456">
-                </div>
-
-                <div>
-                    <p class="error hidden">
-                        Email can't be empty
-                        <!-- 
-                        -password can't be empty
-                        -no user found with this email
-                        -password is incorrect
-                        -->
-                    </p>
-                </div>
-
-                <div>
-                    <input class="form-btn" type="submit" value="Log in">
-                </div>
-
-                <div>
-                    <a class="switch" href="#">Forgot password?</a>
-                </div>
-
-                <div class="form-switch">
-                    <p>No account yet? <a class="forgot" href="#">Register</a></p>
-                </div>
-            </form>
+        
+        <div class="logo-row">
+            <img src="./assets/Folioo.png" alt="Foolio Logo" class="logo">
         </div>
-    </div>
+        
+            <div class="form-container">
+                <form action="" method="post" class="form">
+                    <div class="container">    
+                        <div>
+                            <h3 class="form-title">Log in</h3>
+
+                            <div class="form-field">
+                                <div>
+                                    <label class="form-label" for="email">Email</label>
+                                </div>
+                                <div class="flex">
+                                    <input name="email" autocomplete="off" class="form-input" type="email" placeholder="someone@thomasmore.be">
+                                </div>
+                            </div>
+
+                            <div class="form-field">
+                                <div>
+                                    <label class="form-label" for="password">Password</label>
+                                </div>
+                                <div class="flex">
+                                    <input name="password" autocomplete="off" class="form-input" type="password" placeholder="123456">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <div>
+                                <p class="error hidden">
+                                    Email can't be empty
+                                    <!-- 
+                                    -password can't be empty
+                                    -no user found with this email
+                                    -password is incorrect
+                                    -->
+                                </p>
+                            </div>
+
+                            <div class="flex">
+                                <input class="form-btn" type="submit" value="Log in">
+                            </div>
+
+                            <div class="center">
+                                <a class="switch" href="#">Forgot password?</a>
+                            </div>
+
+                            
+                            <div class="switch-container">
+                                <p class="center line">No account yet? <a class="switch" href="register.php">Register</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -12,53 +12,73 @@
 </head>
 
 <body class="form-background">
-    <div>
-        <!-- <img src="./assets/Folioo.png" alt="Foolio Logo" class="logo"> -->
+    
+        <div class="logo-row">
+            <img src="./assets/Folioo.png" alt="Foolio Logo" class="logo">
+        </div>
 
-        <div class="form">
-            <form action="" method="post">
-                <h3 class="form-title">Register</h3>
+        <div class="form-container">
+            <form action="" method="post" class="form">
+                <div class="container">
+                    <div>
+                        <h3 class="form-title">Register</h3>
 
-                <div class="form-field">
-                    <label class="form-label" for="username">Username</label>
-                    <input name="username" autofocus="" autocomplete="off" class="form-input" type="text" placeholder="den-David">
-                </div>
+                        <div class="form-field">
+                            <div>
+                                <label class="form-label" for="username">Username</label>
+                            </div>
+                            <div class="flex">
+                                <input name="username" autofocus="" autocomplete="off" class="form-input" type="text" placeholder="den-David">
+                            </div>
+                        </div>
 
-                <div class="form-field">
-                    <label class="form-label" for="email">Email</label>
-                    <input name="email" autocomplete="off" class="form-input" type="email" placeholder="someone@thomasmore.be">
-                </div>
+                        <div class="form-field">
+                            <div>
+                                <label class="form-label" for="email">Email</label>
+                            </div>
+                            <div class="flex">
+                                <input name="email" autocomplete="off" class="form-input" type="email" placeholder="someone@thomasmore.be">
+                            </div>
+                        </div>
 
-                <div class="form-field">
-                    <label class="form-label" for="password">Password</label>
-                    <input name="password" autocomplete="off" class="form-input" type="password" placeholder="123456">
-                </div>
+                        <div class="form-field">
+                            <div>
+                                <label class="form-label" for="password">Password</label>
+                            </div>
+                            <div class="flex">
+                                <input name="password" autocomplete="off" class="form-input" type="password" placeholder="123456">
+                            </div>
+                        </div>
+                    </div>
 
-                <div>
-                    <p class="error hidden">
-                        Email has to contain @thomasmore.be or @student.thomasmore.be
-                        <!-- 
-                        Email can't be empty.
-                        This email has already been used.
-                        Username can't be empty
-                        Username is already taken. 
-                        Password can't be empty
-                        Password must be at least 6 characters.
-                        No user found with this email...
-                        -->
-                    </p>
-                </div>
+                    <div>
+                        <div>
+                            <p class="error hidden">
+                                Email has to contain @thomasmore.be or @student.thomasmore.be
+                                <!-- 
+                                Email can't be empty.
+                                This email has already been used.
+                                Username can't be empty
+                                Username is already taken. 
+                                Password can't be empty
+                                Password must be at least 6 characters.
+                                No user found with this email...
+                                -->
+                            </p>
+                        </div>
 
-                <div >
-                    <input class="form-btn" type="submit" value="Register">
-                </div>
+                        <div class="flex">
+                            <input class="form-btn" type="submit" value="Register">
+                        </div>
 
-                <div class="form-switch">
-                    <p>Already an account?<a class="switch"href="#"> Log in</a></p>
+                        <div class="switch-container">
+                            <p class="center line">Already an account? <a class="switch" href="login.php">Log in</a></p>
+                        </div>
+                    </div>
                 </div>
             </form>
         </div>
-    </div>
+
 </body>
 
 </html>
