7457d65ae2a76dbdf0cbe224a5f4bf7644301977
MarieSerroyen <marie.serroyen@gmail.com>
2022-04-27 15:50:07 +0200

    feat: can search on tags

--- classes/Post.php
+++ classes/Post.php
@@ -171,7 +171,7 @@ class Post implements iPost
 
     public static function searchPosts($search){
         $conn = DB::getInstance();
-        $query = $conn->prepare("SELECT count(id) FROM posts WHERE title LIKE :keyword;");
+        $query = $conn->prepare("SELECT count(id) FROM posts WHERE title LIKE :keyword or tags LIKE :keyword;");
         $query->bindValue(':keyword', '%'.$search. '%');
         $query->execute();
         $postId = intval($query->fetchColumn());
@@ -179,7 +179,7 @@ class Post implements iPost
         if($postId !== 0) {            
             //return $postId;
             $conn = DB::getInstance();
-            $statement = $conn->prepare("SELECT * FROM posts WHERE title LIKE :keyword;");
+            $statement = $conn->prepare("SELECT * FROM posts WHERE title LIKE :keyword or tags LIKE :keyword;");
             $statement->bindValue(':keyword', '%'.$search. '%');
             $statement->execute();
 
