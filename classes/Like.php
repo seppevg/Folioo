055506c0a42e00865a8eff2e70400fcce6b28988
MarieSerroyen <marie.serroyen@gmail.com>
2022-05-05 16:12:19 +0200

    feat: created save like function

--- classes/Like.php
+++ classes/Like.php
@@ -1,4 +1,54 @@
 <?php
 class Like implements iProject
 {
+    private $userId; 
+    private $postId;
+
+    /**
+     * Get the value of userId
+     */ 
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set the value of userId
+     *
+     * @return  self
+     */ 
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of postId
+     */ 
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+
+    /**
+     * Set the value of postId
+     *
+     * @return  self
+     */ 
+    public function setPostId($postId)
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+
+    public function Save(){
+        $conn = DB::getInstance();
+        $statement = $conn->prepare("INSERT INTO likes (post_id, user_id) VALUES (:postId, :userId);");
+        $statement->bindValue(':postId', $this->postId);
+        $statement->bindValue(':userId', $this->userId);
+        return $statement->execute();
+    }
 }
